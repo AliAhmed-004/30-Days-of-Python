4727261466b3357dc6bd264a146cf5002fb5f2b8
AliAhmed-004 <eliehmed908@gmail.com>
2024-08-02 23:37:49 +0500

    Finished Day 17

---
+++ Day17.py
@@ -0,0 +1,14 @@
+# Day 17 of 30 Days of Python
+
+# -----------------
+# Exercises Level 1
+# -----------------
+
+# Part 1: Unpacking first 5 countries
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+
+*nordic_countries, es, ru = names
+
+print(nordic_countries)
+print(es)
+print(ru)
